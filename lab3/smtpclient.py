cd9732da5a7c77d22ab2bc45e4c80d4c77d7c720
cs144 <foo@bar.com>
2018-11-30 14:30:57 -0800

    assignmet 3

--- lab3/smtpclient.py
+++ lab3/smtpclient.py
@@ -1,13 +1,20 @@
+'''
+Chris Holland
+V00876844
+'''
+
 from socket import *
 
 msg = "\r\n I love computer networks!"
 endmsg = "\r\n.\r\n"
 
 # Choose a mail server (e.g. Google mail server) and call it mailserver
-mailserver = ?????????
+mailserver = "smtp.uvic.ca"
+port = 25
 
 # Create socket called clientSocket and establish a TCP connection with mailserver
-???????
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((mailserver, 25))
 
 recv = clientSocket.recv(1024).decode()
 print(recv)
@@ -23,20 +30,39 @@ if recv1[:3] != '250':
     print('250 reply not received from server.')
     
 # Send MAIL FROM command and print server response.
-??????
+print "Send Email from..."
+sender = raw_input()
+MAILFROM = "MAIL FROM: <" + sender + ">\r\n"
+clientSocket.send(MAILFROM.encode()) 
+recv2 = clientSocket.recv(1024).decode()
+print recv2
 
 # Send RCPT TO command and print server response. 
-??????
-
+print "Send Email to..."
+receiver = raw_input()
+RCPTO = "RCPT TO: <" + receiver + ">\r\n"
+clientSocket.send(RCPTO.encode())
+recv3 = clientSocket.recv(1024).decode()
+print recv3
 
 # Send DATA command and print server response. 
-??????
+DATA = "DATA\r\n"
+clientSocket.send(DATA.encode())
+recv4 = clientSocket.recv(1024).decode()
+print recv4
 
-# Send message data.
-??????
+# Send message data
+clientSocket.send(msg.encode())
 
 # Message ends with a single period.
-???????
-
+clientSocket.send(endmsg.encode())
+recv5 = clientSocket.recv(1024).decode()
+print recv5.decode()
 # Send QUIT command and get server response.
-???????
+QUIT = "QUIT\r\n"
+clientSocket.send(QUIT.encode())
+
+recv6 = clientSocket.recv(1024).decode()
+print recv6
+
+clientSocket.close()
